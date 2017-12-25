f48f66474b9a75bb8e2cc6689a223e7cb0071768
Nadin-A <nadezhda.trofimova@faceit-team.com>
2017-12-25 11:40:22 +0200

    categories helper and languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,48 +5,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :track_action
-  protect_from_forgery with: :null_session, if: ->{request.format.json?}
+  protect_from_forgery with: :null_session, if: -> { request.format.json? }
   before_action :set_locale
 
+
   def default_url_options
-    { locale: I18n.locale }
+    {locale: I18n.locale}
   end
-
-  #
-  # def set_locale
-  #   I18n.locale = params[:locale]|| I18n.default_locale
-  # end
-
 
   def set_locale
-    valid_locales = %w[en ru]
-    # if params[:locale].present? && valid_locales.include?(params[:locale])
-    #   I18n.locale=params[:locale]
-    #   current_user.update_attribute(:locale, I18n.locale) if user_signed_in?
-    # elsif user_signed_in? && valid_locales.include?(current_user.locale)
-    #   I18n.locale=current_user.locale.to_sym
-    # else
-    #   I18n.locale=I18n.default_locale
-    # end
-
-    #прогрузить пользовтаельскую после логина
-    if user_signed_in?
-      if current_user.locale.present?
-        I18n.locale=current_user.locale.to_sym
-      end
-      if params[:locale].present? && valid_locales.include?(params[:locale])
-        I18n.locale=params[:locale]
-        current_user.update_attribute(:locale, I18n.locale)
-      end
-    else
-      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-      I18n.locale = locale_from_language_header
-      logger.debug "* Locale set to '#{I18n.locale}'"
-    end
+      I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  protected
 
+  protected
 
   def locale_from_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
@@ -61,18 +33,18 @@ class ApplicationController < ActionController::Base
 
   def track_action
     action_type =
-      case "#{controller_path}##{action_name}"
-      when 'comments#create'
-        'comment'
-      when 'likes#create'
-        'like'
-      when 'likes#destroy'
-        'unlike'
-      when 'devise/sessions#create'
-        'user sign in'
-      else
-        'navigation'
-      end
+    case "#{controller_path}##{action_name}"
+    when 'comments#create'
+      'comment'
+    when 'likes#create'
+      'like'
+    when 'likes#destroy'
+      'unlike'
+    when 'devise/sessions#create'
+      'user sign in'
+    else
+      'navigation'
+    end
     ahoy.track "#{request.original_url}", params: request.path_parameters, action_type: action_type if user_signed_in?
   end
 end
---
+++ app/helpers/categories_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module CategoriesHelper
+
+  def set_cover
+    if @category.cover.present?
+      @category.cover
+    else
+      'https://hdwallsource.com/img/2014/6/mountain-peaks-wallpaper-hd-33593-34349-hd-wallpapers.jpg'
+    end
+  end
+
+end
