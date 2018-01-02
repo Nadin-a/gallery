bf24319ddafb2439e40f22aefcbe62ae6734cd17
Nadin-A <nadezhda.trofimova@faceit-team.com>
2018-01-02 13:43:36 +0200

    settings

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,2 @@
-if Rails.env == 'production'
-  redis = Redis.new(url: ENV["REDISCLOUD_URL"])
-else
-  redis = Redis.new
-end
-
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
