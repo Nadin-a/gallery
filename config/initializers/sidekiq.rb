2eb0e9c0aa9bad1f1c92a2ae2a9065bc3db420b6
Nadin-A <nadezhda.trofimova@faceit-team.com>
2018-01-03 15:22:48 +0200

    procfile

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,10 +11,6 @@ if Rails.env.development?
   end
 end
 
-Sidekiq.configure_server do |config|
-  ActiveRecord::Base.configurations[Rails.env.to_s]['pool'] = 30
-end
-
 if Rails.env.production?
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV["REDISTOGO_URL"]}
