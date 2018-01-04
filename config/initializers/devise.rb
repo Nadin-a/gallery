fe5476dd38256f07053da6945a20deb8a1d738d8
Nadin-A <nadezhda.trofimova@faceit-team.com>
2018-01-04 17:43:50 +0200

    without devise backgrounder

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
-  config.mailer = 'DeviseBackgrounder'
+  config.mailer = 'DeviseBackgrounder'  unless Rails.env.production?
 
   # Configure the class responsible to send e-mails.
   # config.user_mailer = 'Devise::Mailer'
