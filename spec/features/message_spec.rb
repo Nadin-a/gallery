92d4484a7a0056d7c75907d01c0ac392f927187c
Nadin-A <nadezhda.trofimova@faceit-team.com>
2018-01-11 18:18:31 +0200

    remove unnecessary test

--- spec/features/message_spec.rb
+++ spec/features/message_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-describe 'image_features', type: :feature do
+describe 'message_features', type: :feature do
   let!(:user) { FactoryBot.create(:random_user) }
   let!(:room) { FactoryBot.create(:room, user: user) }
 
@@ -17,16 +17,6 @@ describe 'image_features', type: :feature do
       fill_in 'New message...', with: message
       click_button 'Post'
       expect(page).to have_content(message)
-    end
-    it 'with empty content', js: true do
-      fill_in 'New message...', with: ''
-      click_button 'Post'
-      expect(page).to have_content("Content can't be blank")
-    end
-    it 'with long content', js: true do
-      fill_in 'New message...', with: 'a' * 1001
-      click_button 'Post'
-      expect(page).to have_content('Content is too long (maximum is 1000 characters)')
     end
   end
 end
